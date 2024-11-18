4654575df6bec10420ea9faffbd4a7efcec5ee27
isabela souza <125027572+isa1307@users.noreply.github.com>
2024-11-18 09:16:55 -0300

    bla

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,16 @@
 import Header from "./components/header/header";
-
+import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <Header />
+        <Header />
+    <div className={styles.all}>
       <h1>Home</h1>
+      
+      <div className={styles.square}>
+        <h1>iyefuhcsd</h1>
+      </div>
+    </div>
     </div>
   );
 }
