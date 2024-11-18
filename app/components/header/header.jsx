a01f7d2b63024e9a081d718677b84609be260999
Guilherme Augusto De Godoy Sachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2024-11-18 10:40:22 -0300

    feat: new page

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -3,7 +3,15 @@ import styles from './page.module.css';
 const Header = () => {
     return (
         <header className={styles.headerContainer}>
+            <div className={styles.logoContainer}>
             <h1>Header</h1>
+            </div>
+            <div className={styles.searchContainer}>
+            <span>Home</span>
+            <span>Noticias</span>
+            <span>Home</span>
+            <span>Noticias</span>
+            </div>
         </header>
     );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,18 @@
+'use client';
 import Header from "./components/header/header";
-
+import style from "./page.module.css";
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1>Home</h1>
+      <h1 className={style.title}>PARCERIA SESI - SENAI - Educação, Conscientização e Prevenção contra Golpes Virtuais.</h1>
+      <p className={style.text}>
+      Com alegria e satisfação que recebemos sua visita em nosso site de Proteção contra Golpes Virtuais de parece SESI - SENAI que tem como objetivo essencial promover a educação, conscientização e prevenção contra golpes virtuais,
+
+
+
+Na era digital, a segurança online é fundamental. Cada vez mais, os golpistas utilizam técnicas sofisticadas para enganar e roubar informações pessoais e financeiras.
+</p>
     </div>
   );
 }
