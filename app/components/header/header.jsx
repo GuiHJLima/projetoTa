28e71080d449967a07c9608802400de325753a2a
Guilherme Augusto De Godoy Sachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2024-11-18 08:40:10 -0300

    feat: new header

---
+++ app/components/header/header.jsx
@@ -0,0 +1,10 @@
+'use client';
+import styles from './page.module.css';
+const Header = () => {
+    return (
+        <header className={styles.headerContainer}>
+            <h1>Header</h1>
+        </header>
+    );
+}
+export default Header;
