bc0622655763781915e7e2b962eb24de2c2deb29
unknown <guilhermesachinelli.564@gamil.com>
2024-11-24 15:09:58 -0300

    feat : new fecht

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,40 +1,46 @@
 "use client";
 import Header from '../components/header/header';
 import style from './page.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Quiz() {
   const [questions, setQuestions] = useState([]);
   const [quizStarted, setQuizStarted] = useState(false);
 
-  const startQuiz = async () => {
-    setQuizStarted(true);
-    const response = await fetch('http://localhost:3003/quiz-aleatorios');
-    const data = await response.json();
-    setQuestions(data);
-  };
-
+  useEffect(() => {
+    fechtQuestions();
+  }, []);
+const fechtQuestions = async () => {
+  try {
+    const response = await fetch('http://localhost:3003/quiz-aleatorios', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Erro ao buscar perguntas');
+    }
+    const result = await response.json();
+    setQuestions(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
   return (
     <div className={style.container}>
       <Header />
       <div className={style.card}>
         <h1 className={style.title}>Quiz</h1>
-        {!quizStarted ? (
-          <>
             <p className={style.info}>Aqui você consegue achar perguntas sobre as matérias lidas.</p>
             <p className={style.info}>Clique no botão abaixo pra iniciar o quiz.</p>
-            <button className={style.button} onClick={startQuiz}>Start Quiz</button>
-          </>
-        ) : (
-          questions.map((question, index) => (
-            <div key={index} className={style.questionCard}>
-              <h2 className={style.question}>{question.pergunta}</h2>
-              
-              <div className={style.answers}>
-              </div>
+          {questions.map((question) => (
+            <div key={question.id} className={style.question}>
+              <h3>{question.question}</h3>
+
             </div>
           ))
-        )}
+        }
       </div>
     </div>
   );
